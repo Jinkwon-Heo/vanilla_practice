f0b92c5380ef8ef32d1195f0a881c94f7ec2125c
kkomabono <jkw8282@naver.com>
2021-12-02 19:18:31 +0900

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,10 +2,13 @@ for(i=1; i<101; i++){
     if(i%3===0){
         if(i%5===0){
             console.log("FizzBuzz");
+            continue;
         }
         console.log("Fizz");
+        continue;
     }else if(i%5===0){
         console.log("Buzz");
+        continue;
     }
     console.log(i);
 }
