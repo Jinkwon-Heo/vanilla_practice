16592a574c98f99bea2d3aefeb26f1fb0e4e32af
kkomabono <jkw8282@naver.com>
2021-12-02 19:26:13 +0900

    Update FizzBuzz.js
    처음에는 3의배수와 5의배수 숫자와 fizz buzz가 같이 출력되었다.
    continue문을 써서 한번 if문을 실행시키면 다시 for조건식으로 돌아가도록 만들었다.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,8 @@
-for(i=1; i<101; i++){
-    if(i%3===0){
-        if(i%5===0){
+for(i=1; i<101; i++){ //for문으로 1~100까지 찍어준다.
+    if(i%3===0){ //첫번째 if문으로 3의배수를 인식하고 fizz를 찍는다.
+        if(i%5===0){ //3의배수의 if문 안에 if문을 중첩시켜서 15의 배수를 구하고 fizzbuzz를 찍는다.
             console.log("FizzBuzz");
-            continue;
+            continue; //continue문으로 3의배수와 5의배수의 숫자는 출력되지 않고 for조건식으로 돌아간다.
         }
         console.log("Fizz");
         continue;
@@ -10,5 +10,5 @@ for(i=1; i<101; i++){
         console.log("Buzz");
         continue;
     }
-    console.log(i);
+    console.log(i); //3의 배수와 5의 배수를 제외한 숫자를 찍어준다.
 }
