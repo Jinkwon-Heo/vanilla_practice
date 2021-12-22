2cb276f5324ff77fc7ba0327f85b63033dc7f085
kkomabono <jkw8282@naver.com>
2021-12-23 07:42:58 +0900

    Number_Baslball

--- Number_Baseball/Number_Baseball.js
+++ Number_Baseball/Number_Baseball.js
@@ -1,8 +1,9 @@
 const button = document.getElementById("startbutton");
-let span = document.querySelector(".result");
 const start = document.querySelector(".start");
 const numberinput = document.getElementById("numberinput");
 const inputDiv = document.querySelector(".inputDiv");
+const result = document.querySelector(".result");
+const restartbutton = document.getElementById("restartbutton");
 
 start.textContent = "버튼을 누르면 숫자야구가 시작됩니다."
 
@@ -93,22 +94,78 @@ button.addEventListener("click", function buttonclick() {
     // }
 });
 
+
+// for(let count=0; count<10; count++){
+//     (function(m){
+// numberinput.addEventListener("keypress", function pressenter(e){
+//     var strike=0;
+//     var ball=0;
+//     var user_number = new Array(0);
+//     var userInputvalue = document.getElementById("numberinput").value;
+//     if(e.key === "Enter"){
+//         if(userInputvalue.length !==3){
+//             alert("잘못된 숫자를 입력하였습니다. 세 자리 숫자를 입력해주세요");
+//             // numberinput.value = "";
+//             // console.log(count);
+//             console.log(userInputvalue);
+//             console.log("이 메세지가 나오면 안됩니다");
+//         }else{
+//             for(let i=0; i<userInputvalue.length; i++){
+//                 user_number.push(userInputvalue[i]);} //비교하기 위해 형태를 배열로 바꿈
+//                 for(let i=0; i<gen_number.length; i++){
+//                     for(let j=0; j<user_number.length; j++){
+//                         if(i===j){
+//                             if(gen_number[i] === user_number[j]){
+//                                 strike++;
+//                                 if(strike ===3){
+//                                     alert("3스트라이크! 게임종료");
+//                                     inputDiv.style.display = "none";
+//                                     restartbutton.style.display = "inline-block";
+//                                 }
+//                             }
+//                         }else if(i!==j){
+//                             if(gen_number[i] === user_number[j]){
+//                                 ball++;
+//                             }
+//                         }
+//                     }
+//                 }
+//                 const addresult = document.createElement("li");
+//                 addresult.textContent = `${m+1}회 | 입력 된 숫자 = ${user_number} 생성 된 숫자 = ${gen_number}|| ${strike}스트라이크 ${ball}볼    !`;
+//                 document.body.appendChild(addresult);
+//                 console.log(count);
+//                 }
+//     }
+// })
+// })(count);
+// }
+
+var count = 0;
 numberinput.addEventListener("keypress", function pressenter(e){
     var strike=0;
     var ball=0;
     var user_number = new Array(0);
+    var userInputvalue = document.getElementById("numberinput").value;
     if(e.key === "Enter"){
-        var userInput = document.getElementById("numberinput").value;
-        if(userInput.length !==3){
+        if(userInputvalue.length !==3){
             alert("잘못된 숫자를 입력하였습니다. 세 자리 숫자를 입력해주세요");
+            numberinput.value = "";
+            console.log(userInputvalue);
+            console.log("이 메세지가 나오면 안됩니다");
         }else{
-            for(let i=0; i<userInput.length; i++){
-                user_number.push(userInput[i]);}
+            for(let i=0; i<userInputvalue.length; i++){
+                user_number.push(userInputvalue[i]);} //비교하기 위해 형태를 배열로 바꿈
+                console.log(user_number);
                 for(let i=0; i<gen_number.length; i++){
                     for(let j=0; j<user_number.length; j++){
                         if(i===j){
                             if(gen_number[i] === user_number[j]){
                                 strike++;
+                                if(strike ===3){
+                                    alert("3스트라이크! 축하합니다");
+                                    inputDiv.style.display = "none";
+                                    restartbutton.style.display = "inline-block";
+                                }
                             }
                         }else if(i!==j){
                             if(gen_number[i] === user_number[j]){
@@ -117,7 +174,17 @@ numberinput.addEventListener("keypress", function pressenter(e){
                         }
                     }
                 }
-        }
-        alert(`입력 된 숫자 = ${user_number} 생성 된 숫자 = ${gen_number}|| ${strike}스트라이크 ${ball}볼`);
+                const addresult = document.createElement("li");
+                addresult.textContent = `${count+1}회 | 입력 된 숫자 = ${user_number} 생성 된 숫자 = ${gen_number}|| ${strike}스트라이크 ${ball}볼    !`;
+                document.body.appendChild(addresult);
+                console.log(count);
+                numberinput.value = "";
+                count++;
+                if(count===10){
+                    alert("10회시도 종료! 아쉽네요");
+                    inputDiv.style.display = "none";
+                    restartbutton.style.display = "inline-block";
+                }
+                }
     }
 })
